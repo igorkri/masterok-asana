b9363f07f1b919a19db3a144d34a3ffd32945111
Igor Krivoshey <igorkri26@gmail.com>
2025-03-25 11:37:44 +0200

    stop all running Docker containers before starting services

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #sudo docker-compose up -d
+
+# останавливаем все контейнеры docker
+sudo docker stop $(sudo docker ps -a -q)
+
 sudo systemctl stop apache2 && systemctl stop mysql && docker-compose up -d && xdg-open http://masterok-asana.loc/admin
