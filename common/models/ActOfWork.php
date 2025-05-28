f5dddd987afb461e3e71ae9eafdc1a7607b5cc0c
Igor Krivoshey <igorkri26@gmail.com>
2025-05-28 13:08:50 +0300

    remove debug statement from period data processing in ActOfWork

--- common/models/ActOfWork.php
+++ common/models/ActOfWork.php
@@ -104,7 +104,7 @@ class ActOfWork extends \yii\db\ActiveRecord
             'month' => $periodsArr[1] ?? '-',
             'year' => $periodsArr[2] ?? '-',
         ];
-        
+
         return
             (self::$monthsList[$periodData['month']] ?? '-') . ' ' .
             ($periodData['year'] ?? '-') . ' (' .
